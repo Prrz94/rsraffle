fe5914d0947d44c44449ea8a5ef8667357ad984e
Prrz94 <75644892+Prrz94@users.noreply.github.com>
2020-12-08 15:31:43 -0600

    Update winner.py

--- winner.py
+++ winner.py
@@ -1,4 +1,10 @@
 import random
-lines = open('raffle_nn.txt').read().splitlines()
+with open('raffle_nn.txt','r') as source:
+    data = [ (random.random(), line) for line in source ]
+data.sort()
+with open('raffle_shuffled.txt','w') as target:
+    for _, line in data:
+        target.write( line )
+lines = open('raffle_shuffled.txt').read().splitlines()
 win = random.choice(lines)
 print("winner: " + win)
